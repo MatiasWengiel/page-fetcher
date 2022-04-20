198492566a00580af43f9ff496a1b2cf9a05aa02
Matias Wengiel <matiaswengiel@gmail.com>
2022-04-20 15:03:32 -0500

    Added functionality so that it will return an error if the file path is invalid

--- fetcher.js
+++ fetcher.js
@@ -1,14 +1,22 @@
-let arguments = process.argv.slice(2);
+let userInput = process.argv.slice(2);
 const request = require('request');
 const fs = require('fs');
+const readline = require('readline');
 
-let website = arguments[0];
-let filePath = arguments[1];
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let website = userInput[0];
+let filePath = userInput[1];
+
 
 const fileWriter = (body) => {
   fs.writeFile(filePath, body, err => {
-    if (err) {
-      console.log(err);
+    if (err && err.errno === -2) {
+      console.log('Supplied path is invalid. Exiting file');
+      process.exit();
       return;
     }
   });
@@ -24,11 +32,13 @@ const fileWriter = (body) => {
       counter += 1;
     }
     console.log(`Downloaded and saved ${counter} bytes to ${filePath}`);
-  
+
+    process.exit();
   });
-}
+};
 
 request(website, (error, response, body) => {
+  
   if (error) {
     console.log('error', error);
   }
@@ -40,10 +50,21 @@ request(website, (error, response, body) => {
   
   fs.access('./index.html', fs.constants.R_OK, (err) => {
     if (err) {
-      fileWriter(body)
+      fileWriter(body);
     } else {
-      console.log('File exists')
+      rl.question(`${filePath} already exists. Overwrite? (Y/N)`, (response) => {
+        if (response === 'Y' || response === 'y') {
+          fileWriter(body);
+          rl.close();
+        } else if (response === 'N' || response === 'n') {
+          console.log('File will not be overwritten');
+          rl.close();
+        } else {
+          console.log('Invalid input, file will not be overwritten');
+          rl.close();
+        }
+      });
     }
-  })
+  });
 
 });
