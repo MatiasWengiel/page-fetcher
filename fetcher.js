33edadc30243151beb9f5b5ff7eced4c54289bf0
Matias Wengiel <matiaswengiel@gmail.com>
2022-04-20 14:32:12 -0500

    Separated function for writing file, and added functionality so that it only writes a file if the file does not previously exist

--- fetcher.js
+++ fetcher.js
@@ -1,40 +1,49 @@
 let arguments = process.argv.slice(2);
 const request = require('request');
 const fs = require('fs');
-const {stat} = require('fs').stat
 
 let website = arguments[0];
 let filePath = arguments[1];
+
+const fileWriter = (body) => {
+  fs.writeFile(filePath, body, err => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+  });
+
+  let counter = 0;
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  
+    for (let eachChar of data) {
+      counter += 1;
+    }
+    console.log(`Downloaded and saved ${counter} bytes to ${filePath}`);
+  
+  });
+}
 
 request(website, (error, response, body) => {
   if (error) {
     console.log('error', error);
   }
   if (response.statusCode !== 200) {
-    console.log('requestStatus', response && response.statusCode)
+    console.log('requestStatus', response && response.statusCode);
   }
+
   
-  fs.writeFile(filePath, body, err => {
+  
+  fs.access('./index.html', fs.constants.R_OK, (err) => {
     if (err) {
-      console.log(err);
-      return
+      fileWriter(body)
+    } else {
+      console.log('File exists')
     }
   })
 
-  let counter = 0
-fs.readFile(filePath, 'utf8', (err, data) => {
-  if (err) {
-    console.error(err)
-    return
-  }
-  
-  for (let eachChar of data) {
-    counter += 1;
-  }
-  console.log(`Downloaded and saved ${counter} bytes to ${filePath}`)
-  
-})
 });
-
-
-
